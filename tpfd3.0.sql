2f311a9428eb16adf1134b1bab9da4df56be777f
guoguo <632522043@qq.com>
2020-02-19 17:13:22 +0800

    3.0desc

--- tpfd3.0.sql
+++ tpfd3.0.sql
@@ -1,7 +1,22 @@
+/*
+Navicat MySQL Data Transfer
 
+Source Server         : localhost_3306
+Source Server Version : 50553
+Source Host           : localhost:3306
+Source Database       : tpfd3.0
+
+Target Server Type    : MYSQL
+Target Server Version : 50553
+File Encoding         : 65001
+
+Date: 2020-02-19 17:12:54
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `wf_form`
+-- Table structure for `wf_fb`
 -- ----------------------------
 DROP TABLE IF EXISTS `wf_fb`;
 CREATE TABLE `wf_fb` (
@@ -16,14 +31,15 @@ CREATE TABLE `wf_fb` (
   `add_time` int(11) DEFAULT NULL,
   `status` int(11) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of wf_form
+-- Records of wf_fb
 -- ----------------------------
+INSERT INTO `wf_fb` VALUES ('1', 'testflow', 'test', 'all', '0', '1', null, null, '1582093871', '0');
 
 -- ----------------------------
--- Table structure for `wf_form_function`
+-- Table structure for `wf_fb_fun`
 -- ----------------------------
 DROP TABLE IF EXISTS `wf_fb_fun`;
 CREATE TABLE `wf_fb_fun` (
@@ -38,7 +54,7 @@ CREATE TABLE `wf_fb_fun` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of wf_form_function
+-- Records of wf_fb_fun
 -- ----------------------------
 
 -- ----------------------------
